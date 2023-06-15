93370f223d6eb1a098f04fe021e7883eb836c95d
Samuel Silva <samuel7silva@outlook.com>
2023-06-15 15:48:18 -0300

    feat: create categories routes

---
+++ src/routes/categories.ts
@@ -0,0 +1,96 @@
+import { FastifyInstance } from 'fastify'
+import { prisma } from '../lib/prisma'
+
+interface CreateCategoriesRequest {
+  name: string
+  id: number
+}
+
+export async function categoriesRoutes(app: FastifyInstance) {
+  app.addHook('preHandler', async (request) => {
+    await request.jwtVerify()
+  })
+
+  app.get('/categories', async (request, reply) => {
+    const categories = await prisma.category.findMany({
+      orderBy: {
+        createdAt: 'asc',
+      },
+    })
+    reply.code(200).send(categories)
+    return categories
+  })
+
+  app.post('/new-category', async (request, reply) => {
+    const { name } = request.body as CreateCategoriesRequest
+
+    const categoryName = await prisma.category.findFirst({
+      where: {
+        name,
+      },
+    })
+    if (categoryName) {
+      reply.code(409).send({ message: 'Categoria já existe' })
+      return
+    }
+
+    try {
+      const newCategory = await prisma.category.create({
+        data: {
+          name,
+        },
+      })
+
+      reply.code(201).send(newCategory)
+    } catch (err) {
+      reply.code(500).send({ message: 'Erro ao criar categoria' })
+    }
+  })
+
+  app.put('/category/:id', async (request, reply) => {
+    const { name } = request.body as CreateCategoriesRequest
+    const { id } = request.params as CreateCategoriesRequest
+    const idNumber = Number(id)
+    if (!id) return
+
+    const categoryName = await prisma.category.findFirst({
+      where: {
+        name,
+      },
+    })
+    if (categoryName) {
+      reply.code(409).send({ message: 'Categoria já existe' })
+      return
+    }
+
+    try {
+      const editCategory = await prisma.category.update({
+        where: {
+          id: idNumber,
+        },
+        data: {
+          name,
+        },
+      })
+      reply.code(201).send(editCategory)
+    } catch (err) {
+      reply.code(500).send({ message: 'Erro ao editar categoria' })
+    }
+  })
+
+  app.delete('/category/:id', async (request, reply) => {
+    const { id } = request.params as CreateCategoriesRequest
+    const idNumber = Number(id)
+    try {
+      await prisma.category.delete({
+        where: {
+          id: idNumber,
+        },
+      })
+
+      reply.code(201).send()
+    } catch (err) {
+      reply.code(500).send({ message: 'Erro ao apagar categoria' })
+    }
+  })
+}
--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
 import { userRoutes } from './routes/user'
 import { authRoutes } from './routes/auth'
+import { categoriesRoutes } from './routes/categories'
 
 const app = fastify()
 app.register(cors, {
@@ -18,6 +19,7 @@ app.get('/users', async (request) => {
 
 app.register(userRoutes)
 app.register(authRoutes)
+app.register(categoriesRoutes)
 
 app.register(jwt, {
   secret: `${process.env.JWT_SECRET}`,
