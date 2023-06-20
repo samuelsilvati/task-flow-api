b3a063df28c36207161f3e6ad93c8f5d6dc97cf1
Samuel Silva <samuel7silva@outlook.com>
2023-06-20 17:43:25 -0300

    feat: add email sending service to forget the pass

--- src/routes/forgotPass.ts
+++ src/routes/forgotPass.ts
@@ -1,6 +1,7 @@
 import { FastifyInstance } from 'fastify'
 import { prisma } from '../lib/prisma'
 import { hash } from 'bcrypt'
+import client from '@sendgrid/mail'
 
 interface AuthUserRequest {
   email: string
@@ -37,11 +38,65 @@ export async function forgotPassRoutes(app: FastifyInstance) {
         expiresIn: '600000',
       },
     )
-    reply
-      .code(200)
-      .send(
-        `${process.env.CLIENT_BASE_URL}/reset-password?token=${token}&id=${id}`,
-      )
+
+    client.setApiKey(`${process.env.SENDGRID_API_KEY}`)
+    const message = {
+      to: `${userAlreadyExists.email}`,
+      from: `${process.env.SENDGRID_FROM_EMAIL}`,
+      subject: 'Redefinição de senha do Task Flow naoresponder@taskflow.com',
+      text: 'Olá, este é um exemplo de email usando o SendGrid.',
+      html: `<html>
+              <head>
+                <title>Recuperação de Senha</title>
+                <style>
+                  body {
+                    font-family: Arial, sans-serif;
+                  }
+                  .container {
+                    max-width: 600px;
+                    margin: 0 auto;
+                    padding: 20px;
+                  }
+                  .button {
+                    display: inline-block;
+                    background-color: #007bff;
+                    color: "white";
+                    padding: 10px 20px;
+                    text-decoration: none;
+                    font-weight: bold;
+                    border-radius: 4px;
+                  }
+                </style>
+              </head>
+              <body>
+                <div class="container">
+                  <h2>Recuperação de Senha</h2>
+                  <p>Olá &nbsp;${userAlreadyExists.name}!</p>
+                  <p>Recebemos uma solicitação para redefinição de senha associada à sua conta. Para continuar o processo de recuperação de senha, clique no botão abaixo para redefinir sua senha:</p>
+                  <a href="${process.env.CLIENT_BASE_URL}/reset-password?token=${token}&id=${id}" class="button">Redefinir Senha</a>
+                  <p>Caso você não tenha solicitado a recuperação de senha, ignore este e-mail.</p>
+                  <p>Atenciosamente,</p>
+                  <p>Equipe de Suporte</p>
+                </div>
+              </body>
+              </html>`,
+    }
+    client
+      .send(message)
+      .then(() => console.log('Mail sent successfully'))
+      .catch((error) => {
+        if (
+          error.response &&
+          error.response.body &&
+          error.response.body.errors
+        ) {
+          const errors = error.response.body.errors
+          console.log(errors)
+        } else {
+          console.log(error)
+        }
+      })
+    reply.code(200).send({ message: 'Mail sent successfully' })
   })
 
   app.post('/reset-password', async (request, reply) => {
