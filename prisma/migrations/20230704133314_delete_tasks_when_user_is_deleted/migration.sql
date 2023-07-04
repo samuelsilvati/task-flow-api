7b46f214a7abb823bcecca043ed26a2ce4c4eb54
Samuel Silva <samuel7silva@outlook.com>
2023-07-04 11:30:47 -0300

    fix: delete tasks when user is deleted

---
+++ prisma/migrations/20230704133314_delete_tasks_when_user_is_deleted/migration.sql
@@ -0,0 +1,19 @@
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Task" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "createdAt" DATETIME,
+    "updatedAt" DATETIME,
+    "isChecked" BOOLEAN DEFAULT true,
+    "categoryId" INTEGER NOT NULL,
+    "userId" TEXT,
+    CONSTRAINT "Task_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "Task_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+INSERT INTO "new_Task" ("categoryId", "createdAt", "description", "id", "isChecked", "name", "updatedAt", "userId") SELECT "categoryId", "createdAt", "description", "id", "isChecked", "name", "updatedAt", "userId" FROM "Task";
+DROP TABLE "Task";
+ALTER TABLE "new_Task" RENAME TO "Task";
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
